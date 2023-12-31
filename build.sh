e5d5612a2457ba72defbf967cd979889b3c9f38e
seb <sebbeck@protonmail.com>
2023-12-31 00:35:52 +0000

    'qwerty'

--- build.sh
+++
@@ -1,5 +0,0 @@
-pushd /mnt/c/Users/seb/code/zmk/app
-source ../venv/bin/activate
-time west build -b nice_nano_v2 -- -DSHIELD=kyria_left -DZMK_CONFIG="/mnt/c/Users/seb/code/zmk-config/config"
-time west build -b nice_nano_v2 -- -DSHIELD=kyria_right -DZMK_CONFIG="/mnt/c/Users/seb/code/zmk-config/config"
-popd
